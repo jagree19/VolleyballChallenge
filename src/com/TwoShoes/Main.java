700eb8b98b5e862e07bf57e8f02c802b10b1fbbe
jagree19 <asujagree19@gmail.com>
2019-10-09 14:33:56 -0600

    completed project

--- src/com/TwoShoes/Main.java
+++ src/com/TwoShoes/Main.java
@@ -3,11 +3,14 @@ package com.TwoShoes;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
 	
 	static ArrayList<Player> playerSet = new ArrayList<>();
+	static ArrayList<Team> teamsSet = new ArrayList<>();
 	
 
 	public static void main(String[] args) throws IOException {
@@ -15,6 +18,11 @@ public class Main {
 		Menu();
 	}
 	
+	/**
+	 * Prints out the menu and handles the choices made
+	 * 
+	 * @throws IOException
+	 */
 	public static void Menu() throws IOException {
 		System.out.println("Please make a choice: Enter the number of your choice: ");
 		System.out.println("1: Open a roster");
@@ -24,16 +32,17 @@ public class Main {
 		System.out.println("5: Quit");
 		
 		Scanner scan = new Scanner(System.in);
+		Scanner file = new Scanner(System.in);
 		int choice = scan.nextInt();
 		
 		switch(choice) {
 			case 1:
 				System.out.println("Please enter the file where the roster is stored.\n");
-				Scanner file = new Scanner(System.in);
 				String fileName = file.nextLine();
 				playerSet = getRoster(fileName);
 				System.out.println("The roster from " + fileName + " was opened successfully.");
 				System.out.println("");
+//				file.close();
 				Menu();
 				break;
 			case 2:
@@ -45,13 +54,37 @@ public class Main {
 				}
 				System.out.println("");
 				Menu();
+				break;
 			case 3:
 				isRosterOpen();
+				System.out.println("Teams based on attack stats");
+				System.out.println("");
+				MakeAttackTeams();
+				Menu();
+				break;
+			case 4:
+				isRosterOpen();
+				System.out.println("Teams based on block stats");
+				System.out.println("");
+				MakeBlockTeams();
+				Menu();
+				break;
+			default:
+				System.out.println("Goodbye");
+				scan.close();
+				file.close();
+				break;
 				
 		}
 	}
 	
+	/**
+	 * Checks to make sure a roster has been read in before allowing processing
+	 * Alerts the user if the roster has not been read in yet and returns to the menu
+	 * @throws IOException
+	 */
 	public static void isRosterOpen() throws IOException {
+		
 		if(playerSet.size() == 0) {
 			System.out.println("Please open a roster first.");
 			System.out.println();
@@ -60,6 +93,15 @@ public class Main {
 	
 	}
 	
+	/**
+	 * Reads a roster in from a text file
+	 * 
+	 * @param fileName the filename as input by the user
+	 * 
+	 * @return playerSet - the roster imported from the text file
+	 * 
+	 * @throws IOException
+	 */
 	public static ArrayList<Player> getRoster(String fileName) throws IOException {
 		
 		Scanner inFile = new Scanner(new File(fileName));
@@ -73,5 +115,142 @@ public class Main {
 		}
 		
 		return playerSet;	
+	}
+	
+	/**
+	 * This method makes teams based on the attack stat and prints it out
+	 * 
+	 * @throws IOException
+	 */
+	public static void MakeAttackTeams() throws IOException {
+		
+		int numberOfPlayers = playerSet.size(); // total # of players on roster
+		int numberOfTeams = numberOfPlayers / 6; // how many teams of 6 we can make
+		int playerIndex = numberOfTeams * 6; // the number of players put onto the teams
+		
+		Team team1 = new Team();
+		Team team2 = new Team();
+		Team team3 = new Team();
+		Team team4 = new Team();
+		
+		ArrayList<Team> teams = new ArrayList<>();
+		
+		
+		// Sorts the players by their attack stats in descending order.
+		Collections.sort(playerSet, new Comparator<Player>() {
+			@Override
+			public int compare(Player p1, Player p2) {
+				return p2.getAttackStat().compareTo(p1.getAttackStat());
+			}
+		});
+		
+		int teamIndex = 0; // to keep track of which team we add to
+		
+		/**
+		 * this counter will increment when we have added a player to each team
+		 * when it reaches 6 (The size of a team) it will trigger the end of making teams
+		 * ex: if we have 2 teams - we will increment it when we add a player to Team 2
+		 */
+		int counter = 0; // increments when all teams have another player added
+		while(counter < 6) {
+			for(int i = 0; i < playerIndex; i++) {
+				switch(teamIndex) {
+				case 0:
+					team1.addPlayer(playerSet.get(i), counter);
+					break;
+				case 1:
+					team2.addPlayer(playerSet.get(i), counter);
+					break;
+				case 2:
+					team3.addPlayer(playerSet.get(i), counter);
+					break;
+				case 3:
+					team4.addPlayer(playerSet.get(i), counter);
+					break;
+				default:
+					break;
+				}
+				if(teamIndex == (numberOfTeams - 1)) {
+					teamIndex = 0;
+					counter++;
+				}else {
+					teamIndex++;
+				}
+			}	
+		}
+		System.out.println(team1);
+		System.out.println(team2);
+		if(team3.getPlayer(0) != null){
+			System.out.println(team3);
+		}
+		if(team4.getPlayer(0) != null){
+			System.out.println(team4);
+		}
+	}
+	
+	public static void MakeBlockTeams() throws IOException {
+			
+		int numberOfPlayers = playerSet.size(); // total # of players on roster
+		int numberOfTeams = numberOfPlayers / 6; // how many teams of 6 we can make
+		int playerIndex = numberOfTeams * 6; // the number of players put onto the teams
+		
+		Team team1 = new Team();
+		Team team2 = new Team();
+		Team team3 = new Team();
+		Team team4 = new Team();
+		
+		ArrayList<Team> teams = new ArrayList<>();
+		
+		
+		// Sorts the players by their block stats in descending order.
+		Collections.sort(playerSet, new Comparator<Player>() {
+			@Override
+			public int compare(Player p1, Player p2) {
+				return p2.getBlockStat().compareTo(p1.getBlockStat());
+			}
+		});
+		
+		int teamIndex = 0; // to keep track of which team we add to
+		
+		/**
+		 * this counter will increment when we have added a player to each team
+		 * when it reaches 6 (The size of a team) it will trigger the end of making teams
+		 * ex: if we have 2 teams - we will increment it when we add a player to Team 2
+		 */
+		int counter = 0; // increments when all teams have another player added
+		while(counter < 6) {
+			for(int i = 0; i < playerIndex; i++) {
+				switch(teamIndex) {
+				case 0:
+					team1.addPlayer(playerSet.get(i), counter);
+					break;
+				case 1:
+					team2.addPlayer(playerSet.get(i), counter);
+					break;
+				case 2:
+					team3.addPlayer(playerSet.get(i), counter);
+					break;
+				case 3:
+					team4.addPlayer(playerSet.get(i), counter);
+					break;
+				default:
+					break;
+				}
+				if(teamIndex == (numberOfTeams - 1)) {
+					teamIndex = 0;
+					counter++;
+				}else {
+					teamIndex++;
+				}
+			}	
+		}
+		System.out.println(team1.toString());
+		System.out.println(team2.toString());
+		if(team3.getPlayer(0) != null){
+			System.out.println(team3.toString());
+		}
+		if(team4.getPlayer(0) != null){
+			System.out.println(team4.toString());
+		}
 	}
 }
--- src/com/TwoShoes/Team.java
+++ src/com/TwoShoes/Team.java
@@ -1,11 +1,34 @@
 package com.TwoShoes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Team {
 	
-	Player[] team;
+	Player[] team = new Player[6];
 	
 	public Team() {
 		//default constructor
+	}
+	
+	public void addPlayer(Player player, int index) {
+		team[index] = player;
+	}
+	
+	public int length() {
+		return team.length;
+	}
+	
+	public Player getPlayer(int index){
+		return team[index];
+	}
+	
+	public String toString(){
+		String str = "";
+		for(int i = 0; i < team.length; i++){
+			str += (team[i].toString() + "\n");
+		}
+		return str;
 	}
 
 }
